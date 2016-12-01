069d7b02efc763869df6f78fa6408f9a593ed6c3
Steven Zhang <steven.shutao@gmail.com>
2016-12-01 18:38:59 +0800

    working now

--- tc.py
+++ tc.py
@@ -4,7 +4,7 @@
 # format required by Advent Geneva system for quick import.
 # 
 
-import csv
+import csv, argparse, glob, os, sys
 from trade_converter.utility import logger, get_current_path, get_record_fields
 from trade_converter.port_12307 import convert12307
 
@@ -27,9 +27,17 @@ def get_converter(portfolio_id):
 
 
 
-def write_csv(file, records):
-	logger.debug('write_csv(): for portfolio {0}'.format(portfolio_id))
+def get_all_trade_files(directory):
+	"""
+	Get all trade files under a directory. Here we assume all .xls files
+	under the directory are trade files. Other files and sub directories
+	are ignored.
+	"""
+	return glob.glob(directory+'\\*.xls')
 
+
+
+def write_csv(file, records):
 	with open(file, 'w', newline='') as csvfile:
 		logger.debug('write_csv(): {0}'.format(file))
 		file_writer = csv.writer(csvfile)
@@ -60,12 +68,31 @@ def write_csv(file, records):
 
 
 if __name__ == '__main__':
-	portfolio_id = '12307'
-	file = get_current_path() + '\\samples\\12307-20161111.xls'
-	files = [file]
+	parser = argparse.ArgumentParser(description='Read portfolio trades and create a Geneva trade upload file.')
+	parser.add_argument('portfolio_id')
+	parser.add_argument('--folder', help='folder in current directory containing multiple trade files', required=False)
+	parser.add_argument('--file', help='input trade file', required=False)
+	args = parser.parse_args()
 
-	do_convert = get_converter(portfolio_id)
+	if not args.file is None:
+		files = [get_current_path() + '\\' + args.file]
+	elif not args.folder is None:
+		folder = get_current_path() + '\\' + args.folder
+		if not os.path.exists(folder) or not os.path.isdir(folder):
+			print('{0} is not a valid directory'.format(folder))
+			sys.exit(1)
+
+		files = get_all_trade_files(folder)
+	else:
+		print('Please provide either --file or --folder input')
+		sys.exit(1)
+
+	do_convert = get_converter(args.portfolio_id)
 	records = do_convert(files)
 
-	output_file = get_current_path() + '\\trades_upload.csv'
+	output_file = get_current_path() + '\\trade_upload.csv'
 	write_csv(output_file, records)
+
+	# directory = get_current_path() + '\\samples'
+	# files = get_all_trade_files(directory)
+	# print(len(files))
